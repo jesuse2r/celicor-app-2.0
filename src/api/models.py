5db4bb1446820f342c60e235c22dfc169b8210b6
jesuse2r <jesuse2rr@gmail.com>
2023-04-27 14:49:46 +0000

    endpoint: login y register, modelo clase usuario, roles admin y buyer

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
 
+class Role(enum.Enum):
+    admin="admin"
+    buyer="buyer"
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(50),nullable= False)
+    document_id= db.Column(db.String(50), nullable= False, unique=True)
+    phone= db.Column(db.String(50), nullable= False, unique=True)
+    address= db.Column(db.String(120), nullable= False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(260), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    role = db.Column(db.Enum(Role), nullable=False, default="buyer")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +24,13 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "document_id": self.document_id,
+            "phone": self.phone,
+            "address": self.address,
             "email": self.email,
+
             # do not serialize the password, its a security breach
         }
+
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,79 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Role
+from flask_jwt_extended import create_access_token, jwt_required
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
+@api.route('/user', methods=['POST'])
+def register():
+    body = request.json
+    email = body.get('email', None)
+    password = body.get('password', None)
+    is_active = True
+    name = body.get('name', None)
+    address = body.get('address', None)
+    document_id = body.get('document_id', None)
+    phone = body.get('phone', None)
+
+    role = body.get('role', None)
+    if email is None or password is None or name is None or  address is None or document_id is None or phone is None:
+        return{"error": "todos los campos son requeridos"}, 400
+
+    if role not in  Role.__members__:
+        return{"error": f"{role} No existe en los roles"}
+    password_hash = generate_password_hash(password)
+   
+    new_user = User(email=email, password=password_hash, is_active=is_active, name=name, address=address, document_id=document_id, phone=phone, role="buyer" )
+    db.session.add(new_user)
+    try: 
+        db.session.commit()
+        return jsonify({"msg":"usuario creado con exito"})
+    except Exception as error:
+        return jsonify({"error": error.args[0]})
+
+@api.route("/user/login", methods=["POST"])
+
+def login():
+
+    body= request.json
+    email = body.get("email", None)
+    password = body.get("password", None)
+    if email is None or password is None:
+        return {"error": "Todos los campos son requeridos"}, 400
+    login_user = User.query.filter_by(email=email).first()
+    if not login_user:
+        return {"error": "Usuario no encontrado"}, 404
+    if check_password_hash(login_user.password, password):
+        token = create_access_token({"id": login_user.id})
+        print(token)
+        
+        
+        return jsonify({"access_token": token})
+    
+    else:
+         return "contraseña incorrecta", 401
+    
+    
+
+    
+
+@api.route('/user', methods=['GET'])
+@jwt_required()
 def handle_hello():
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "hola manao"
     }
 
+
     return jsonify(response_body), 200
+
+
+
+
