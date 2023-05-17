2bcfa314e099ea688355e8f64f960f0382f9a9d0
Santiagoarraez <Santiagoarraez@gmail.com>
2023-05-17 21:52:27 +0000

    Update styling

--- src/front/js/component/CreditView.jsx
+++ src/front/js/component/CreditView.jsx
@@ -6,57 +6,47 @@ import ButtonPagar from "./ButtonPagar.jsx";
 
 const CreditView = () => {
     return (
-        <>
-            <div className="bg-light m-5 w-25 h-75 " >
-                <div className="card-body ">
-                    <div className="justify-content-between d-flex " >
-                        <img src={logo} className="card-img-top celicor " alt="..." />
-                        <img src={visaLogo} className="card-img-top  celicor" alt="..." />
+        <div className="text-center">
+            <div className="bg-light m-5 w-100 h-75" >
+                <div className="card-body">
+                    <div className="d-flex justify-content-around">
+                        <img src={logo} className="card-img-top celicores blue" alt="..." />
+                        <img src={visaLogo} className="card-img-top licomovil" alt="..." />
                     </div>
-                    <form className="row  needs-validation m-5  ">
-                        <div className="col-md-12 position-relative">
-                            <label htmlFor="validationTooltip01" className="form-label">Nombre en la tarjeta</label>
-                            <input type="text" className="form-control" id="validationTooltip01" required />
+                    <span className="d-flex justify-content-around">
+                        <div>
+                            <form className="row  needs-validation m-5  ">
+                                <div className="col-md-6 position-relative">
+                                    <label htmlFor="validationTooltip01" className="form-label">Nombre en la tarjeta</label>
+                                    <input type="text" className="form-control" id="validationTooltip01" required />
+                                </div>
+                                <div className="col-md-6 position-relative">
+                                    <label htmlFor="validationTooltip02" className="form-label">Número de la tarjeta de crédito</label>
+                                    <input type="text" className="form-control" id="validationTooltip02" required />
+                                </div>
+                                <div className="col-md-6 position-relative">
+                                    <label htmlFor="validationTooltip03" className="form-label">Vencimiento</label>
+                                    <input type="text" className="form-control" id="validationTooltip03" required />
+                                </div>
+                                <div className="col-md-6 position-relative ">
+                                    <label htmlFor="validationTooltip05" className="form-label">CVV</label>
+                                    <input type="text" className="form-control" id="validationTooltip05" required />
+                                </div>
+                                <div className="col-12 mt-3 justify-content-center d-flex">
+                                    <button className="btn btn-primary " type="submit">Pagar</button>
+                                </div>
+                            </form>
+                            <div className="display-6 d-flex justify-content-center gap-5">
+                                <a href="https://api.whatsapp.com/send/?phone=4142776795&text&type=phone_number&app_absent=" className="card-link text-success "><i className="fab fa-whatsapp"></i></a>
+                                <ButtonPagar />
+                            </div>
                         </div>
-                        <div className="col-md-12 position-relative">
-                            <label htmlFor="validationTooltip02" className="form-label">Número de la tarjeta de crédito</label>
-                            <input type="text" className="form-control" id="validationTooltip02" required />
-                        </div>
-                        <div className="col-md-6 position-relative">
-                            <label htmlFor="validationTooltip03" className="form-label">Vencimiento</label>
-                            <input type="text" className="form-control" id="validationTooltip03" required />
-                        </div>
-                        <div className="col-md-6 position-relative ">
-                            <label htmlFor="validationTooltip05" className="form-label">CVV</label>
-                            <input type="text" className="form-control" id="validationTooltip05" required />
-                        </div>
-                        <div className="col-12 mt-3 justify-content-center d-flex">
-                            <button className="btn btn-primary " type="submit">Pagar</button>
-                        </div>
-                    </form>
-                    <p>Por favor enviar captura de pantalla.</p>
-                    <div className=" display-6">
-                        <a href="https://api.whatsapp.com/send/?phone=4142776795&text&type=phone_number&app_absent=" className="card-link text-success  "><i className=" whatssapLogo  fab fa-whatsapp"></i></a>
-                        <ButtonPagar/>
-                    </div>
-
+                    </span>
                 </div>
             </div>
-        </>
-
-
-
-
-
-
-
-
+        </div>
     )
 }
-
-
-
-
 
 
 export default CreditView
--- src/front/js/component/PaypalView.jsx
+++ src/front/js/component/PaypalView.jsx
@@ -4,26 +4,33 @@ import logo from "../../img/logo.png"
 import ButtonPagar from "./ButtonPagar.jsx";
 
 const PaypalView = () => {
-    return (
-        <>
-        <div className=" bg-light m-5 w-25 h-75 ">
-        <div className="justify-content-between d-flex">
-        <img src={logo} className="card-img-top celicores m-2" alt="..." />
-        <img src={paypalLogo} className="card-img-top celicores" alt="..."/>
-        </div>
+  return (
+    <>
+      <div className="bg-light m-5 w-100 h-75" >
         <div className="card-body">
-          <h6 className="card-subtitle mb-2 text-body-secondary">Correo: Jesuse2rr@gmail.com</h6>
-          <p className="card-text">Nombre: Jesús Rodríguez</p>
-          <p className="card-text">Número  de teléfono: 04242187006</p>
-          <p>Por favor enviar captura de pantalla.</p>
-        <div className="display-6">
-          <a href="https://api.whatsapp.com/send/?phone=4142776795&text&type=phone_number&app_absent=" className="card-link text-success"><i className="fab fa-whatsapp"></i></a>
-          <ButtonPagar/>
+          <div className="d-flex justify-content-around ">
+            <img src={logo} className="card-img-top celicores blue" alt="..." />
+            <img src={paypalLogo} className="card-img-top licomovil" alt="..." />
           </div>
+          <span className="d-flex justify-content-around">
+            <div>
+              <h3 className="card-title mt-2">Paypal</h3>
+              <span className="fs-5">
+                <p className="card-subtitle mb-2 text-body-secondary">Correo: Jesuse2rr@gmail.com</p>
+                <p className="card-text">Nombre: Jesús Rodríguez</p>
+                <p className="card-text">Número de teléfono: 04242187006</p>
+                <p>Por favor enviar captura de pantalla.</p>
+              </span>
+              <div className="display-6 d-flex justify-content-between">
+                <a href="https://api.whatsapp.com/send/?phone=4142776795&text&type=phone_number&app_absent=" className="card-link text-success "><i className="fab fa-whatsapp"></i></a>
+                <ButtonPagar />
+              </div>
+            </div>
+          </span>
         </div>
       </div>
-      </>
-    )
+    </>
+  )
 }
 
 
